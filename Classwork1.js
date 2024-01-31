b0bfb4ad23c1cbc114029dae8a7dbc0a55cd73df
dasaribharathkumar6 <157642165+dasaribharathkumar6@users.noreply.github.com>
2024-01-31 13:20:19 -0500

    Create Classwork1.js

---
+++ Classwork1.js
@@ -0,0 +1,26 @@
+//Query 1
+db.details.insertOne({
+    "id": "POST_ID",
+    "title": "TITLE_OF_POST",
+    "description": "POST_DESCRIPTION",
+    "by": "POST_BY",
+    "url": "URL_OF_POST",
+    "tags": ["TAG1", "TAG2", "TAG3"],
+    "likes": "TOTAL_LIKES",
+    "comments": [
+        {
+            "user": "COMMENT_BY",
+            "message": "TEXT",
+            "dateCreated": "DATE_TIME",
+            "like": "LIKES"
+        },
+        {
+            "user": "COMMENT_BY",
+            "message": "TEXT",
+            "dateCreated": "DATE_TIME",
+            "like": "LIKES"
+        }
+    ]
+}
+)
+
